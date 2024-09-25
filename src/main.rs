a70ac6d357f674c6f8c465f173bedf18b0ef97af
joyanedel <joyanede@alumnos.inf.utfsm.cl>
2024-09-24 22:16:52 -0300

    Add total words as parameter

--- src/main.rs
+++ src/main.rs
@@ -12,25 +12,24 @@ mod tui;
 mod types;
 mod views;
 
-const TARGET_SENTENCE_LENGTH: usize = 25;
-
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg(short, long, default_value = "./dictionaries/default.txt")]
     dictionary_dir: String,
+    #[arg(short, long, default_value = "25")]
+    total_words: usize,
 }
 
 fn main() -> io::Result<()> {
     // setup terminal
-    let mut terminal = tui::init()?;
     let args = Args::parse();
+    let mut terminal = tui::init()?;
 
     let dictionary = read_to_string(args.dictionary_dir).unwrap();
     let dictionary = dictionary.lines().collect();
 
-    let target_word =
-        pick_random_words_from_dictionary(&dictionary, TARGET_SENTENCE_LENGTH).join(" ");
+    let target_word = pick_random_words_from_dictionary(&dictionary, args.total_words).join(" ");
 
     // Typing playground
     let mut typing_playground = TypingPlayground::new(target_word.clone());
